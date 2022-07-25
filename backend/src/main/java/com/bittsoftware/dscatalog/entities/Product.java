25c7e3b27a9927f9acbfeba0ff107adf4ccc909c
rodrigobittencourtlima <rodrigo.bittencourt.lima@gmail.com>
2022-07-25 11:14:08 -0300

    Product category association and seeding script

--- backend/src/main/java/com/bittsoftware/dscatalog/entities/Product.java
+++ backend/src/main/java/com/bittsoftware/dscatalog/entities/Product.java
@@ -11,6 +11,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -23,13 +26,18 @@ public class Product implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	@Column(columnDefinition = "TEXT")
 	private String description;
+	
 	private Double price;
 	private String imgUrl;
 
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant date;
 
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	Set<Category> categories = new HashSet<>();
 
 	public Product() {
