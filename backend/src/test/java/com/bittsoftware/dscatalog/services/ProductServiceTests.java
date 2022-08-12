aa2c20542205bdcb10b96f8aea2b5276ba7d015d
rodrigobittencourtlima <rodrigo.bittencourt.lima@gmail.com>
2022-08-12 11:27:15 -0300

    More product service tests

--- backend/src/test/java/com/bittsoftware/dscatalog/services/ProductServiceTests.java
+++ backend/src/test/java/com/bittsoftware/dscatalog/services/ProductServiceTests.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import com.bittsoftware.dscatalog.repositories.ProductRepository;
+import com.bittsoftware.dscatalog.services.exceptions.DatabaseException;
 import com.bittsoftware.dscatalog.services.exceptions.ResourceNotFoundException;
 
 import org.junit.jupiter.api.Assertions;
@@ -14,6 +15,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
@@ -27,13 +30,16 @@ public class ProductServiceTests {
 
 	private Long existingId;
 	private Long nonExistingId;
+	private Long dependentId;
 
 	@BeforeEach
 	void setUp() throws Exception {
 		existingId = 1L;
 		nonExistingId = 1000L;
+		dependentId = 4L;
 		doNothing().when(repository).deleteById(existingId);
-		doThrow(ResourceNotFoundException.class).when(repository).deleteById(nonExistingId);
+		doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);
+		doThrow(DataIntegrityViolationException.class).when(repository).deleteById(dependentId);
 	}
 
 	@Test
@@ -46,6 +52,12 @@ public class ProductServiceTests {
 	public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExists() {
 		Assertions.assertThrows(ResourceNotFoundException.class, () -> service.delete(nonExistingId));
 		verify(repository, times(1)).deleteById(nonExistingId);
+	}
+
+	@Test
+	public void deleteShouldThrowDatabaseExceptionWhenIntegrityViolationOccurs() {
+		Assertions.assertThrows(DatabaseException.class, () -> service.delete(dependentId));
+		verify(repository, times(1)).deleteById(dependentId);
 	}
 
 }
