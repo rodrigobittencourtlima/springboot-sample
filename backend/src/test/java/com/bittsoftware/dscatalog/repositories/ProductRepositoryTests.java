58b70de856da7009d106ab8ce20beb52b1844dca
rodrigobittencourtlima <rodrigo.bittencourt.lima@gmail.com>
2022-08-12 09:32:41 -0300

    More repository tests

--- backend/src/test/java/com/bittsoftware/dscatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/bittsoftware/dscatalog/repositories/ProductRepositoryTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 @DataJpaTest
 public class ProductRepositoryTests {
@@ -14,8 +15,12 @@ public class ProductRepositoryTests {
 	@Test
 	public void deleteShouldDeleteObjectWhenIdExists() {
 		repository.deleteById(1L);
-
 		Assertions.assertFalse(repository.findById(1L).isPresent());
+	}
+	
+	@Test
+	public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExists() {
+		Assertions.assertThrows(EmptyResultDataAccessException.class, () -> repository.deleteById(100L));
 	}
 
 }
