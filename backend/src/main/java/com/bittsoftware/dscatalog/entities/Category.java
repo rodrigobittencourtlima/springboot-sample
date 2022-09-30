f7536184de3a79728776bc653f3f6ab24310f88c
rodrigobittencourtlima <rodrigo.bittencourt.lima@gmail.com>
2022-09-30 16:27:53 -0300

    Users and roles database seeding

--- backend/src/main/java/com/bittsoftware/dscatalog/entities/Category.java
+++ backend/src/main/java/com/bittsoftware/dscatalog/entities/Category.java
@@ -34,6 +34,7 @@ public class Category implements Serializable {
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant updatedAt;
 
+	@ManyToMany(mappedBy = "categories")
 	private Set<Product> products = new HashSet<>();
 
 	public Category() {
