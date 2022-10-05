49890e11fd32c791d020f3e63b855ff4a36ccf39
rodrigobittencourtlima <rodrigo.bittencourt.lima@gmail.com>
2022-10-05 14:43:53 -0300

    WIP: Beans validation

--- backend/src/main/java/com/bittsoftware/dscatalog/resources/ProductResource.java
+++ backend/src/main/java/com/bittsoftware/dscatalog/resources/ProductResource.java
@@ -2,8 +2,7 @@ package com.bittsoftware.dscatalog.resources;
 
 import java.net.URI;
 
-import com.bittsoftware.dscatalog.dto.ProductDTO;
-import com.bittsoftware.dscatalog.services.ProductService;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -18,6 +17,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.bittsoftware.dscatalog.dto.ProductDTO;
+import com.bittsoftware.dscatalog.services.ProductService;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -37,14 +39,14 @@ public class ProductResource {
 	}
 
 	@PostMapping
-	public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
+	public ResponseEntity<ProductDTO> insert(@Valid @RequestBody ProductDTO dto) {
 		dto = service.insert(dto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
 		return ResponseEntity.created(uri).body(dto);
 	}
 
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto) {
+	public ResponseEntity<ProductDTO> update(@PathVariable Long id, @Valid @RequestBody ProductDTO dto) {
 		dto = service.update(id, dto);
 		return ResponseEntity.ok().body(dto);
 	}
