c413bed3dad311a0c2e4ae8b120fa082853f3b14
rodrigobittencourtlima <rodrigo.bittencourt.lima@gmail.com>
2022-10-05 14:20:09 -0300

    Finished User CRUD

--- backend/src/main/java/com/bittsoftware/dscatalog/entities/User.java
+++ backend/src/main/java/com/bittsoftware/dscatalog/entities/User.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -27,7 +28,10 @@ public class User implements Serializable {
 
 	private String firstName;
 	private String lastName;
+	
+	@Column(unique = true)
 	private String email;
+	
 	private String password;
 
 	@ManyToMany(fetch = FetchType.EAGER)
