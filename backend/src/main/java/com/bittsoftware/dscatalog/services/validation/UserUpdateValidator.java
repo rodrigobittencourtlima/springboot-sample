46fa29419adceee15ebe232962a1e1b89b2a73a7
rodrigobittencourtlima <rodrigo.bittencourt.lima@gmail.com>
2022-10-07 17:59:34 -0300

    UserUpdateValidator

--- backend/src/main/java/com/bittsoftware/dscatalog/services/validation/UserUpdateValidator.java
+++ backend/src/main/java/com/bittsoftware/dscatalog/services/validation/UserUpdateValidator.java
@@ -2,11 +2,14 @@ package com.bittsoftware.dscatalog.services.validation;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
 
 import com.bittsoftware.dscatalog.dto.UserUpdateDTO;
 import com.bittsoftware.dscatalog.entities.User;
@@ -14,6 +17,9 @@ import com.bittsoftware.dscatalog.repositories.UserRepository;
 import com.bittsoftware.dscatalog.resources.exceptions.FieldMessage;
 
 public class UserUpdateValidator implements ConstraintValidator<UserUpdateValid, UserUpdateDTO> {
+
+	@Autowired
+	private HttpServletRequest request;
 
 	@Autowired
 	private UserRepository repository;
@@ -27,8 +33,12 @@ public class UserUpdateValidator implements ConstraintValidator<UserUpdateValid,
 
 		List<FieldMessage> list = new ArrayList<>();
 
+		@SuppressWarnings("unchecked")
+		var uriVars = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		long userId = Long.parseLong(uriVars.get("id"));
+
 		User user = repository.findByEmail(dto.getEmail());
-		if (user != null) {
+		if (user != null && userId != user.getId()) {
 			list.add(new FieldMessage("email", "E-mail já cadastrado"));
 		}
 
